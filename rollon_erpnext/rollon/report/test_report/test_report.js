9d5d9ce5358172b4f0e8d6b86fdfc5c2a8af2410
Manoj Kumar <makwizdev1@gmail.com>
2017-10-20 17:08:50 +0530

    @0.0.4 Missing custom fieds and custom doctypes.

---
+++ rollon_erpnext/rollon/report/test_report/test_report.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
+frappe.query_reports["Test Report"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+			"width": "60px"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "60px"
+		}
+	]
+}
+
